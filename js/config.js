559d5eb58894ed1dd8d6b153aebf44c76b885220
gruponuwek-web <gruponuwek@gmail.com>
2026-06-23 16:12:32 -0600

    cAMBIOS

--- js/config.js
+++ js/config.js
@@ -7,6 +7,6 @@
    · La carpeta de Drive
    ============================================================ */
 
-const API             = 'https://script.google.com/macros/s/AKfycbwgHPaLNg_g6y6g9OzmtvPXjGgdr5A7Rj2-EP3cgw9PaBOhVrTY_6O09USmPFPeVd60/exec';
+const API             = 'https://script.google.com/macros/s/AKfycbyoIdSEK3ISV-gBKWEBQUHJZoIxw-DgO8yBwV7yWvrkXCE7uI77_PctyPnx8WiBsWWY/exec';
 const COORD           = 'Leonardo Hernández';
 const DRIVE_FOLDER_ID = '1wI_hoT-eAo9ANAYlbXdLs4S5gb4lD5Rt';
