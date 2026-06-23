57a0bdca5820f20ee8a7c2e12dc5db88651dddbc
gruponuwek-web <gruponuwek@gmail.com>
2026-06-23 13:34:36 -0600

    modificacion de segunda implemntacion confi

--- js/config.js
+++ js/config.js
@@ -7,6 +7,6 @@
    · La carpeta de Drive
    ============================================================ */
 
-const API             = 'https://script.google.com/macros/s/AKfycbzqKyPEL5LNiNWLY2o_SgecaQLVb3_itUvvW7_k_biRX8Mnr0VUFNGMjqHBhyNi5V4z/exec';
+const API             = 'https://script.google.com/macros/s/AKfycbwvIQl9LIGRllc4YdjTdp3KyXrOVON4kotxPHwkzkT_2OrqPHv97UA-JQf_OhpeKBMW/exec';
 const COORD           = 'Leonardo Hernández';
 const DRIVE_FOLDER_ID = '1wI_hoT-eAo9ANAYlbXdLs4S5gb4lD5Rt';
